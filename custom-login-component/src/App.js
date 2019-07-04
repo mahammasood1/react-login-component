230964ae96c9dbc0922cbc13850f6c8aaae9d36e
Abdul Hannan <abdulhannan8122@gmail.com>
2019-07-04 12:08:26 +0500

    form

--- custom-login-component/src/App.js
+++ custom-login-component/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+function LoginForm() {
   return (
     <div className="App">
+      <form >
+        <label for="email">Email: </label>
+        <input type="email" id="email" placeholder="Enter your email" name="Email"/>
+        <label for="pass">Password: </label>
+        <input type="password" id="pass" placeholder="Enter your password" name="Password"/>
+      </form>
     </div>
   );
 }
 
-export default App;
+export default LoginForm;

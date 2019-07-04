32c37e11f7fcaaae2153917cd6cb18af3a03be08
Abdul Hannan <abdulhannan8122@gmail.com>
2019-07-04 12:12:12 +0500

    comp

--- custom-login-component/src/App.js
+++ custom-login-component/src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './App.css';
 
-function LoginForm() {
-  return (
-    <div className="App">
-      <form >
-        <label for="email">Email: </label>
-        <input type="email" id="email" placeholder="Enter your email" name="Email"/>
-        <label for="pass">Password: </label>
-        <input type="password" id="pass" placeholder="Enter your password" name="Password"/>
-      </form>
-    </div>
-  );
+class LoginForm extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <form >
+          <label for="email">Email: </label>
+          <input type="email" id="email" placeholder="Enter your email" name="Email"/>
+          <label for="pass">Password: </label>
+          <input type="password" id="pass" placeholder="Enter your password" name="Password"/>
+          <input type="submit"/>
+        </form>
+      </div>
+    );
+  }
+  
 }
 
 export default LoginForm;

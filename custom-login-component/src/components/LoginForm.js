57d7384fcc1fb905347392911171d62bfba2b578
Maham <mahammasood31@gmail.com>
2019-07-22 16:15:12 +0500

    latest commit

--- custom-login-component/src/components/LoginForm.js
+++ custom-login-component/src/components/LoginForm.js
@@ -2,6 +2,10 @@ import React from 'react';
 import '../App.css'
 import './base.css';
 import './custom.css';
+import { FormErrors } from '../FormErrors';
+
+// import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+
 
 class LoginForm extends React.Component {
 
@@ -10,30 +14,89 @@ class LoginForm extends React.Component {
         this.state = {
             email: '',
             password: '',
+            formErrors: {email: '', password: ''},
+            isEmailValid: false,
+            isPwdValid: false,
+            isFormValid: false
         };
     }
 
-    handleSubmit = (e) => {
+    handleSubmit = (event) => {
 
         alert(this.state.email);
         alert(this.state.password);
-        e.preventDefault();
+        event.preventDefault();
     }
 
     handleChange = (field) => (event) => {
+        let name = event.target.name;
+        let value = event.target.value;
+
         this.setState({
-            [field]: event.target.value,
-        })
+            [field]: value},  () => {this.validateField(name, value) });
+        
     }
+
+    // emailValidation = (values) => {
+    //     let errors = {};
+    //     if (!values.email) {
+    //         errors.email = 'Email address is required';
+    //       } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+    //         errors.email = 'Email address is invalid';
+    //       }
+    //     return errors;
+    // }
+
+    validateField(name, value) {
+        let formErrors = this.state.formErrors;
+        let emailValid = this.state.isEmailValid;
+        let passwordValid = this.state.isPwdValid;
+      
+        switch(name) {
+          case 'email':
+            emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+            formErrors.email = emailValid ? '' : ' is invalid';
+            break;
+          case 'password':
+            passwordValid = value.length >= 8;
+            formErrors.password = passwordValid ? '': ' is too short';
+            break;
+          default:
+            break;
+        }
+        this.setState({formErrors: formErrors,
+                        isEmailValid: emailValid,
+                        isPwdValid: passwordValid
+                      }, this.formValidation);
+      }
+      
+      formValidation() {
+        this.setState({isFormValid: this.state.isEmailValid && this.state.isPwdValid});
+      }
+
+      errorClass(error) {
+        return(error.length === 0 ? '' : 'has-error');
+     }
 
     render() {
         return (
-            <div>
+            <div id="container">
                 <form onSubmit={this.handleSubmit}>
+                    
                     <h3 className={this.props.fields.classes.title}>{this.props.fields.label}</h3>
+
+                    <div className="panel panel-default">
+                        <FormErrors formErrors={this.state.formErrors} />
+                    </div>
+
                     {this.props.fields.fields.map(field =>(
                         <div>
-                           <input class="input-con" 
+                        <div class="label">
+                            {field.label}
+                        </div>
+
+                        <div>
+                           <input 
                                 type={field.type}
                                 value={this.state[field.name]}
                                 onChange={this.handleChange(field.name)}
@@ -42,11 +105,22 @@ class LoginForm extends React.Component {
                                 name={field.name} 
                             />
                         </div>
+
+                        </div>
                     ))
                     }
-                    <div class="container-submit" className={this.props.fields.classes.button}>
-					<input type="submit"/>  
-                    </div>                     
+
+                    <div id="bottom">
+                    <div className={this.props.fields.classes.button}>
+					<input type="submit" disabled={!this.state.isFormValid}/>  
+                    </div>   
+
+                    <div>
+                    <input type="checkbox"/><label class="check" for="checkbox">Remember Me</label>
+                    </div>
+
+                    </div>   
+
 				</form>
             </div>
         );
--- custom-login-component/src/index.js
+++ custom-login-component/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import LoginForm  from './App';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap-theme.css';
+
 
 ReactDOM.render(<LoginForm />, document.getElementById('root'));
